f9161e9ad6e70e93446116ba48b70c0ba730ebbe
Tobbe <tmauermann@hotmail.com>
2016-01-28 11:04:51 +0100

    Added Login and Register WinForms

--- WCFChatClient/WCFChatClient/ChatOption.Designer.cs
+++ WCFChatClient/WCFChatClient/ChatOption.Designer.cs
@@ -29,68 +29,68 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChatOption));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.pictureBoxChatMan = new System.Windows.Forms.PictureBox();
+            this.pictureBoxChatWoman = new System.Windows.Forms.PictureBox();
+            this.pictureBoxChatUnisex = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChatMan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChatWoman)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChatUnisex)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pictureBoxChatMan
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(11, 40);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(106, 99);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
+            this.pictureBoxChatMan.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxChatMan.Image")));
+            this.pictureBoxChatMan.Location = new System.Drawing.Point(11, 40);
+            this.pictureBoxChatMan.Name = "pictureBoxChatMan";
+            this.pictureBoxChatMan.Size = new System.Drawing.Size(106, 99);
+            this.pictureBoxChatMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxChatMan.TabIndex = 6;
+            this.pictureBoxChatMan.TabStop = false;
             // 
-            // pictureBox2
+            // pictureBoxChatWoman
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(251, 40);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(106, 99);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
+            this.pictureBoxChatWoman.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxChatWoman.Image")));
+            this.pictureBoxChatWoman.Location = new System.Drawing.Point(251, 40);
+            this.pictureBoxChatWoman.Name = "pictureBoxChatWoman";
+            this.pictureBoxChatWoman.Size = new System.Drawing.Size(106, 99);
+            this.pictureBoxChatWoman.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxChatWoman.TabIndex = 7;
+            this.pictureBoxChatWoman.TabStop = false;
             // 
-            // pictureBox3
+            // pictureBoxChatUnisex
             // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(133, 40);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(106, 99);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 8;
-            this.pictureBox3.TabStop = false;
+            this.pictureBoxChatUnisex.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxChatUnisex.Image")));
+            this.pictureBoxChatUnisex.Location = new System.Drawing.Point(133, 40);
+            this.pictureBoxChatUnisex.Name = "pictureBoxChatUnisex";
+            this.pictureBoxChatUnisex.Size = new System.Drawing.Size(106, 99);
+            this.pictureBoxChatUnisex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxChatUnisex.TabIndex = 8;
+            this.pictureBoxChatUnisex.TabStop = false;
             // 
-            // Form1
+            // ChatOption
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(370, 162);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBoxChatUnisex);
+            this.Controls.Add(this.pictureBoxChatWoman);
+            this.Controls.Add(this.pictureBoxChatMan);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
+            this.Name = "ChatOption";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Chat Client";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChatMan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChatWoman)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChatUnisex)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBoxChatMan;
+        private System.Windows.Forms.PictureBox pictureBoxChatWoman;
+        private System.Windows.Forms.PictureBox pictureBoxChatUnisex;
     }
 }
 
---
+++ WCFChatClient/WCFChatClient/Register.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WCFChatClient
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Register));
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.radioButtonMan = new System.Windows.Forms.RadioButton();
+            this.radioButtonWoman = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(12, 39);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(153, 20);
+            this.textBoxUsername.TabIndex = 0;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(9, 23);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(55, 13);
+            this.labelUsername.TabIndex = 1;
+            this.labelUsername.Text = "Username";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(9, 62);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(53, 13);
+            this.labelPassword.TabIndex = 3;
+            this.labelPassword.Text = "Password";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(12, 78);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(153, 20);
+            this.textBoxPassword.TabIndex = 2;
+            // 
+            // radioButtonMan
+            // 
+            this.radioButtonMan.AutoSize = true;
+            this.radioButtonMan.Location = new System.Drawing.Point(15, 12);
+            this.radioButtonMan.Name = "radioButtonMan";
+            this.radioButtonMan.Size = new System.Drawing.Size(46, 17);
+            this.radioButtonMan.TabIndex = 4;
+            this.radioButtonMan.TabStop = true;
+            this.radioButtonMan.Text = "Man";
+            this.radioButtonMan.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonWoman
+            // 
+            this.radioButtonWoman.AutoSize = true;
+            this.radioButtonWoman.Location = new System.Drawing.Point(80, 12);
+            this.radioButtonWoman.Name = "radioButtonWoman";
+            this.radioButtonWoman.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonWoman.TabIndex = 5;
+            this.radioButtonWoman.TabStop = true;
+            this.radioButtonWoman.Text = "Woman";
+            this.radioButtonWoman.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButtonMan);
+            this.panel1.Controls.Add(this.radioButtonWoman);
+            this.panel1.Location = new System.Drawing.Point(12, 104);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(153, 41);
+            this.panel1.TabIndex = 6;
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(92, 151);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(73, 31);
+            this.buttonRegister.TabIndex = 7;
+            this.buttonRegister.Text = "Register";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(12, 151);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(73, 31);
+            this.buttonBack.TabIndex = 8;
+            this.buttonBack.Text = "Back";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(180, 204);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.textBoxUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Register";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.RadioButton radioButtonMan;
+        private System.Windows.Forms.RadioButton radioButtonWoman;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.Button buttonBack;
+    }
+}
