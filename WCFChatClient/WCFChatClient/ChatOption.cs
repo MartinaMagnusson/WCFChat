18b523e11a4d2c1ba1a39536193b0a71d5541da1
Tobbe <tmauermann@hotmail.com>
2016-01-28 10:49:54 +0100

    ChatOption Form Added

--- WCFChatClient/WCFChatClient/ChatOption.cs
+++ WCFChatClient/WCFChatClient/ChatOption.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WCFChatClient
 {
-    public partial class Form1 : Form
+    public partial class ChatOption : Form
     {
-        public Form1()
+        public ChatOption()
         {
             InitializeComponent();
         }
