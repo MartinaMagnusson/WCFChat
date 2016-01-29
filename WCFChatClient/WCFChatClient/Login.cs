bb7a1dbc8fef5c539a8bb2939907389cacdcd903
Rasmus Leifsgård <Leifsgard@outlook.com>
2016-01-29 13:20:34 +0100

    ChatClient tillagd i LoginUser & Updaterade Reference pga key => Password

--- WCFChatClient/WCFChatClient/Login.cs
+++ WCFChatClient/WCFChatClient/Login.cs
@@ -7,14 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WCFChatClient.ChatService;
 
 namespace WCFChatClient
 {
     public partial class Login : Form
     {
+        ChatService.ChatClient _chatClient;
         public Login()
         {
             InitializeComponent();
+            _chatClient = new ChatService.ChatClient();
         }
 
         private void buttonRegister_Click(object sender, EventArgs e)
@@ -25,9 +28,10 @@ namespace WCFChatClient
 
         private void buttonLogin_Click(object sender, EventArgs e)
         {
+           
+            _chatClient.LogInUser(textBoxUsername.Text, textBoxPassword.Text);
 
-
-            if (!String.IsNullOrEmpty(textBoxUsername.Text) || !String.IsNullOrEmpty(textBoxPassword.Text)) 
+            if (!String.IsNullOrEmpty(textBoxUsername.Text) || !String.IsNullOrEmpty(textBoxPassword.Text))
             {
                 this.Hide();
                 ChatroomOptions chatroomOptions = new ChatroomOptions();
